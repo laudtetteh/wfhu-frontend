6337f18e66a9e9ca23bc60d2c6135f2e048f30cd
Tim Ganter <1911000+timganter@users.noreply.github.com>
2021-01-18 15:39:27 -0800

    Add logo/svgs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-// import logo from '../assets/logo.svg'
+import logo from '../assets/logo.svg'
 
 export function App () {
   return (
     <header className='bg-yellow-800'>
       hello!!!!!
-      {/* <img src={logo} /> */}
+      <img src={logo} />
     </header>
   )
 }
