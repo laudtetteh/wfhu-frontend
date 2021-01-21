bf482b24ed8a987d0fce81efc36c7520eab0bb1e
Tim Ganter <1911000+timganter@users.noreply.github.com>
2021-01-20 21:43:18 -0800

    Fix build.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import logo from '../assets/logo.svg'
 
 export function App () {
   return (
-    <header className='bg-yellow-800'>
-      hello!!!!!
-      <img src={logo} />
+    <header className='bg-yellow-400'>
+      {logo()}
     </header>
   )
 }
