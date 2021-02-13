ef8351eeed65d03e2bcba6b37411ab71ee1f819a
Laud Tetteh <laud@studiotenfour.com>
2021-02-13 09:28:04 +0000

    Wrapping up on MailChimp

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+let tailwindcss = require('tailwindcss');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: [
+        tailwindcss('./tailwind.config.js'), // your tailwind.js configuration file path
+        require('autoprefixer'),
+        require('postcss-loader')
+    ]
 }
