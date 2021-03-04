e55993f6ac955b184d863db1e88a706ec23c6d68
Laud Tetteh <laud@studiotenfour.com>
2021-03-04 19:51:56 +0000

    Clean up gif loop and testimonial loop on home page

--- src/components/Elements/RowOfGifs.tsx
+++ src/components/Elements/RowOfGifs.tsx
@@ -4,18 +4,10 @@ import { Card } from "../Card/gif";
 export const RowOfGifs = (props) => {
     const gifs = props.pageBag.row_of_gifs;
 
-    const leftGifsCount = Math.ceil(gifs.length / 5);
-    const leftGifs = gifs.slice(0, leftGifsCount);
-    const rightGifs = gifs.slice(leftGifsCount, gifs.length);
-
     return (
         <React.Fragment>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-                {leftGifs.map((gif, i) => {
-                    return <Card gif={gif.gif} key={`gif__${gif.id}`} />
-                })}
-
-                {rightGifs.map((gif, i) => {
+                {gifs.map((gif, i) => {
                     return <Card gif={gif.gif} key={`gif__${gif.id}`} />
                 })}
             </div>
